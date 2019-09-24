95ee351834a5bb3edf9abd7ab157b676fb1e23b9
Ikey Doherty <ikey.doherty@lispysnake.com>
2019-09-24 08:11:03 +0100

    core: Start working on a TextureCache implementation
    This is a somewhat heavy handed approach that will preallocate around
    400 texture objects in RAM that we can access, while permitting growth.
    An optimisation would be to have a greedy_realloc type of function
    within LsArray.
    
    We'll return handles as uint16_t typedefs for each texture, and globally
    use those. This will allow us to access at O(1) speeds each texture object
    from the array and also handle subregions, etc.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ src/core/texture-cache.c
@@ -0,0 +1,98 @@
+/*
+ * This file is part of lispysnake2d.
+ *
+ * Copyright (c) 2019 Lispy Snake, Ltd.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+
+ */
+
+#include <SDL.h>
+#include <stdlib.h>
+
+#include "libls.h"
+#include "texture-cache.h"
+
+#define DEFAULT_CACHE_SIZE 200
+
+static void ls2d_texture_cache_destroy(Ls2DTextureCache *self);
+
+/**
+ * Opaque Ls2DTextureCache implementation
+ */
+struct Ls2DTextureCache {
+        Ls2DObject object; /*< Parent */
+
+        LsArray *cache; /*< Cache of textures in an array */
+};
+
+/**
+ * We don't yet do anything fancy.
+ */
+Ls2DObjectTable texture_vtable = {
+        .destroy = (ls2d_object_vfunc_destroy)ls2d_texture_cache_destroy,
+        .obj_name = "Ls2DTextureCache",
+};
+
+Ls2DTextureCache *ls2d_texture_cache_new()
+{
+        Ls2DTextureCache *self = NULL;
+
+        self = calloc(1, sizeof(struct Ls2DTextureCache));
+        if (ls_unlikely(!self)) {
+                return NULL;
+        }
+        self->cache = ls_array_new_size(sizeof(void *), DEFAULT_CACHE_SIZE);
+        if (ls_unlikely(!self->cache)) {
+                free(self);
+                return NULL;
+        }
+
+        return ls2d_object_init((Ls2DObject *)self, &texture_vtable);
+}
+
+Ls2DTextureCache *ls2d_texture_cache_unref(Ls2DTextureCache *self)
+{
+        return ls2d_object_unref(self);
+}
+
+/**
+ * Clear out the allocated texture.
+ */
+static void clear_texture(void *v)
+{
+}
+
+static void ls2d_texture_cache_destroy(Ls2DTextureCache *self)
+{
+        if (self->cache) {
+                ls_array_free(self->cache, clear_texture);
+        }
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/core/texture-cache.h
@@ -0,0 +1,56 @@
+/*
+ * This file is part of lispysnake2d.
+ *
+ * Copyright (c) 2019 Lispy Snake, Ltd.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+
+ */
+
+#pragma once
+
+#include "libls.h"
+#include "object.h"
+
+typedef uint16_t Ls2DTextureHandle;
+
+typedef struct Ls2DTextureCache Ls2DTextureCache;
+
+/**
+ * Construct a new Ls2DTextureCache Object
+ */
+Ls2DTextureCache *ls2d_texture_cache_new(void);
+
+/**
+ * Unref an allocated Ls2DTextureCache
+ */
+Ls2DTextureCache *ls2d_texture_cache_unref(Ls2DTextureCache *self);
+
+DEF_AUTOFREE(Ls2DTextureCache, ls2d_texture_cache_unref)
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
